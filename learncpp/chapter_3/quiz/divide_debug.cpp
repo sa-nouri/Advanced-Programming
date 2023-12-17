3c7f48069116d7cb17b3901497ad42af416966a5
Salar Nouri <salr.nouri@gmail.com>
2023-12-17 12:35:31 +0330

    Add divide debugging example

---
+++ learncpp/chapter_3/quiz/divide_debug.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int readNumber()
+{
+	std::cout << "Please enter a number: ";
+	int x {};
+	std::cin >> x;
+	return x;
+}
+
+void writeAnswer(int x)
+{
+	std::cout << "The quotient is: " << x << '\n';
+}
+
+int main()
+{
+	int x{ readNumber() };
+	int y{ readNumber() };
+	writeAnswer(x/y);
+
+	return 0;
+}
