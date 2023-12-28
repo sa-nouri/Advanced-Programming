cef80f3840faf4fbe0310491db07e677ad5b03ee
Salar Nouri <salr.nouri@gmail.com>
2023-12-28 13:07:23 +0330

    Add character initialization instead of int8_t

--- learncpp/chapter_4/type_conversion.cpp
+++ learncpp/chapter_4/type_conversion.cpp
@@ -4,6 +4,7 @@ int main()
 {
     std::cout << "Enter a single character: ";
     int8_t character {};
+    // char character {};
     std::cin >> character;
 
     std::cout << "You Entered '" << character << "', which has ASCII code " << static_cast<int>(character) << ".\n";
