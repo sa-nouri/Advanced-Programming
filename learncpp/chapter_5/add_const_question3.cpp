b29760fea432c3bfb4175cc6d5d41daa56fabd6f
Salar Nouri <salr.nouri@gmail.com>
2024-02-28 10:46:20 +0330

    Add added constant version

--- learncpp/chapter_5/add_const_question3.cpp
+++ learncpp/chapter_5/add_const_question3.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
-// gets tower height from user and returns it
+// This function cannot be made constexpr because output and input can only be done at runtime.
+// The versions of `operator<<` and `operator>>` that do output and input don't support constexpr.
 double getTowerHeight()
 {
 	std::cout << "Enter the height of the tower in meters: ";
@@ -9,19 +10,28 @@ double getTowerHeight()
 	return towerHeight;
 }
 
-// Returns ball height from ground after "seconds" seconds
-double calculateBallHeight(double towerHeight, int seconds)
+// Returns height from ground after "seconds" seconds
+// This function is made constepxr because it just calculates a value from its inputs and return it.
+// Arithmetic can be done at compile-time, and no non-constexpr functions are called.
+// Reminder: A constexpr function can be evaluated at compile-time or runtime.
+//   If its arguments are constexpr, it can be called at compile-time.
+//   In this case, it's called at runtime because the argument for towerHeight isn't constexpr.
+// If a function can be made constexpr, it should be.
+// Remember: function parameters are not constexpr, even in a constexpr function
+constexpr double calculateBallHeight(double towerHeight, int seconds)
 {
-	double gravity{ 9.8 };
+	constexpr double gravity{ 9.8 }; // constexpr because it's a compile-time constant
 
 	// Using formula: [ s = u * t + (a * t^2) / 2 ], here u(initial velocity) = 0
-	double distanceFallen{ (gravity * (seconds * seconds)) / 2.0 };
-	double currentHeight{ towerHeight - distanceFallen };
+	// These variables can't be constexpr since their initializers aren't constant expressions
+	const double distanceFallen{ (gravity * (seconds * seconds)) / 2.0 };
+	const double currentHeight{ towerHeight - distanceFallen };
 
 	return currentHeight;
 }
 
-// Prints ball height above ground
+// This function cannot be made constexpr because output and input can only be done at runtime.
+// The versions of `operator<<` and `operator>>` that do output and input don't support constexpr.
 void printBallHeight(double ballHeight, int seconds)
 {
 	if (ballHeight > 0.0)
@@ -30,17 +40,19 @@ void printBallHeight(double ballHeight, int seconds)
 		std::cout << "At " << seconds << " seconds, the ball is on the ground.\n";
 }
 
-// Calculates the current ball height and then prints it
-// This is a helper function to make it easier to do this
+// This function cannot be made constexpr because output and input can only be done at runtime.
+// The versions of `operator<<` and `operator>>` that do output and input don't support constexpr.
 void printCalculatedBallHeight(double towerHeight, int seconds)
 {
-	double ballHeight{ calculateBallHeight(towerHeight, seconds) };
+	// height can only be const (not constexpr) because its initializer is not a constant expression
+	const double ballHeight{ calculateBallHeight(towerHeight, seconds) };
 	printBallHeight(ballHeight, seconds);
 }
 
 int main()
 {
-	double towerHeight{ getTowerHeight() };
+	// towerHeight can only be const (not constexpr) because its initializer is not a constant expression
+	const double towerHeight{ getTowerHeight() };
 
 	printCalculatedBallHeight(towerHeight, 0);
 	printCalculatedBallHeight(towerHeight, 1);
